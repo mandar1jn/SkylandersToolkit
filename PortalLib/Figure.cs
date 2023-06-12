cc7623efb3c7b455e066ec4a02f1ea2cc8672ae6
Mandar1jn <marijnkneppers@gmail.com>
2023-06-12 21:29:12 +0200

    Expand figure data

--- PortalLib/Figure.cs
+++ PortalLib/Figure.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using Microsoft.VisualBasic;
+using System.Linq;
+using System.Text;
 
 namespace PortalLib
 {
@@ -14,14 +16,6 @@ namespace PortalLib
     {
         byte[][] data = new byte[0x40][];
 
-        public byte Area
-        {
-            get
-            {
-                return (byte)((GetArea1Sequence() < GetArea2Sequence()) ? 0x0 : 0x1);
-            }
-        }
-
         public short ID
         {
             get
@@ -30,11 +24,85 @@ namespace PortalLib
             }
         }
 
-        public byte VariantID
+        public short VariantID
         {
             get
             {
-                return GetByte(0x01, 0x0C);
+                return GetShort(0x01, 0x0C);
+            }
+        }
+
+        public byte BCC
+        {
+            get
+            {
+                return GetByte(0x0, 0x4);
+            }
+            set
+            {
+                SetByte(0x0, 0x4, value);
+            }
+        }
+
+        public uint WebCodeNumber1
+        {
+            get
+            {
+                return GetUInt(0x01, 0x04);
+            }
+        }
+
+        public uint WebCodeNumber2
+        {
+            get
+            {
+                return GetUInt(0x01, 0x08);
+            }
+        }
+
+        public ulong FullWebCodeNumber
+        {
+            get
+            {
+                return ((ulong)WebCodeNumber2 << 32) | WebCodeNumber1;
+            }
+        }
+
+        public string WebCode
+        {
+            get
+            {
+                string[] lookupTable = new string[]
+                {
+                    "2", "3", "4", "5", "6", "7", "8", "9", "B", "C", "D", "F", "G", "H", "J",
+                    "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"
+                };
+
+                decimal cur = new(FullWebCodeNumber);
+                decimal[] values = new decimal[10];
+
+                for(int i = 0; i < values.Length;  i++)
+                {
+                    decimal prev = cur;
+                    cur = Math.Truncate(cur / 29);
+                    values[i] = ((prev / 29m) - cur) *  29m; ;
+                }
+
+                StringBuilder sb = new StringBuilder();
+                for(int i = 9; i > 4; i--)
+                {
+                    sb.Append(lookupTable[Convert.ToUInt32(values[i])]);
+                }
+
+                sb.Append('-');
+
+                for (int i = 4; i >= 0; i--)
+                {
+                    sb.Append(lookupTable[Convert.ToUInt32(values[i])]);
+                }
+
+                return sb.ToString();
+
             }
         }
 
@@ -90,34 +158,22 @@ namespace PortalLib
         {
             return data[block][offset];
         }
+        public void SetByte(int block, int offset, byte value)
+        {
+            data[block][offset] = value;
+        }
         public short GetShort(int block, int offset) { return (short)(data[block][offset] + (data[block][offset + 1] << 8)); }
 
-        public byte GetArea1Sequence()
+        public uint GetUInt(int block, int offset)
         {
-            return GetByte(0x08, 0x09);
-        }
+            uint value = 0;
 
-        public byte GetArea2Sequence()
-        {
-            return GetByte(0x24, 0x09);
-        }
-
-        public byte GetAreaBlock()
-        {
-            return (byte)((Area == 0) ? 0x08 : 0x24);
-        }
-
-        public void LogData()
-        {
-            for (byte i = 0; i < data.Length; i++)
+            for (sbyte i = 3; i >= 0; i--)
             {
-                string dataString = "";
-                for (int j = 0; j < data[i].Length; j++)
-                {
-                    dataString += $" 0x{data[i][j]:X2}";
-                }
-                Console.WriteLine($"Block 0x{i:X2}: {dataString}");
+                value += (uint)data[block][offset + i] << (8 * i);
             }
+
+            return value;
         }
 
         public void Dump(string filePath)
